f46ba2cf34d1e86d001888de13479fea3ecf872f
matteo00111 <matteo.vannucchi11@gmail.com>
2021-11-21 22:37:51 +0100

    Aggiornato file per integrazione con circleCI

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,11 @@ export default {
 	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
 	ssr: false,
 
+	target: 'server',
+
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: 'manager-dashboard',
+		title: 'dashboard',
 		htmlAttrs: {
 			lang: 'en',
 		},
