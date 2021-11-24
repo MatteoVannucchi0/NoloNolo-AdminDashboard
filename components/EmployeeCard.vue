e412916cc6bceb3cf9c0a6fe009d1ceb39c4cae8
matteo00111 <matteo.vannucchi11@gmail.com>
2021-11-24 10:38:36 +0100

    Added a simple display of the employee information in a card component

--- components/EmployeeCard.vue
+++ components/EmployeeCard.vue
@@ -3,15 +3,31 @@
 		<div class="card-title">
 			{{ employee.lastname }} {{ employee.firstname }}
 		</div>
+		<div class="card-image">
+			<img :src="profilePictureUrl">
+		</div>
+		<div v-if="false" id="email">
+			{{ employee.loginInfo.email }}
+		</div>
+		<div>
+			{{ employee.authorization }}
+		</div>
 	</div>
 </template>
 
 <script>
+import api from '../assets/helper/api';
+
 export default {
 	props: {
 		employee: {
 			type: Object,
 			default: () => {},
+		},
+	},
+	computed: {
+		profilePictureUrl() {
+			return api.toServerImageUrl(this.employee.profilePicture);
 		},
 	},
 };
@@ -22,5 +38,17 @@ export default {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+	}
+	.container-flex{
+		align-items: center;
+	}
+
+	.card {
+		padding: 25px;
+	}
+
+	#email {
+		font-size: 1.2em !important;
+
 	}
 </style>
--- pages/employees.vue
+++ pages/employees.vue
@@ -9,10 +9,16 @@
 			</div>
 			<div class="content-container">
 				<b-container fluid>
-					<div id="employeeContainer" class="container-flex">
-						<EmployeeCard v-for="employee in duplicateData" :key="employee._id" :v-if="loaded" :employee="employee" />
+					<div id="employeeContainer" class="container-grid">
+						<EmployeeCard v-for="employee in employees" :key="employee._id" :v-if="loaded" :employee="employee" />
 					</div>
 				</b-container>
+				<b-button v-if="paginator.hasPrevPage" @click="paginatorPrev">
+					Prev
+				</b-button>
+				<b-button v-if="paginator.hasNextPage" @click="paginatorNext">
+					Next
+				</b-button>
 			</div>
 		</div>
 	</div>
@@ -25,40 +31,55 @@ import api from '../assets/helper/api';
 export default {
 	data() {
 		return {
-			employees: {
-				type: Array,
-				default: () => [],
-			},
-			loaded: {
-				type: Boolean,
-				default: false,
+			paginator: {
+				type: Object,
+				default: () => {},
 			},
 		};
 	},
 	computed: {
 		duplicateData() {
-			const makeRepeated = (arr, repeats) => [].concat(...Array.from({ length: repeats }, () => arr));
+			/* const makeRepeated = (arr, repeats) => [].concat(...Array.from({ length: repeats }, () => arr));
+			const data = makeRepeated(this.employees, 50); */
+			/* eslint-disable no-underscore-dangle */
+			/* eslint-disable no-restricted-syntax */
 
-			return makeRepeated(this.employees, 5);
+			/* // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+			for (const e of data) {
+				e._id = Math.random() * (1000000 - 10) + 10;
+			} */
+
+			return undefined;
+		},
+
+		employees() {
+			return this.paginator ? this.paginator.docs : undefined;
+		},
+		loaded() {
+			return !!this.paginator;
 		},
 	},
 	async mounted() {
-		const response = await api.employees.get();
-		this.employees = response.data;
-		console.log(this.employees);
-		this.loaded = true;
+		this.paginator = (await api.employees.get({ limit: 3 })).data;
+	},
+	methods: {
+		async paginatorPrev() {
+			this.paginator = (await api.employees.paginatorPrev(this.paginator)).data;
+		},
+		async paginatorNext() {
+			this.paginator = (await api.employees.paginatorNext(this.paginator)).data;
+		},
 	},
 };
 
 </script>
 
 <style scoped>
-	.card {
-		display: flex;
-		justify-content: center;
-	}
-
-	.container-flex{
-		gap: 20px;
+	.container-grid {
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: auto;
+		justify-content: stretch;
+		column-gap: 15px;
+		row-gap: 5px;
 	}
 </style>
