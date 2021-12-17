fa0c48363a6e691ab5e0c751aedf2a36fb043ae6
matteo00111 <matteo.vannucchi11@gmail.com>
2021-12-17 22:16:20 +0100

    Aggiunto un po di logging per debuggare

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,12 +2,14 @@ import config from '../assets/helper/config';
 
 export default async function ({ redirect, route }) {
 	// Solo nel caso in cui non sia già nella pagina di login effettuo i controlli
+
 	if (route.path === '/login' || route.path === '/login/') {
 		return undefined;
 	}
 
 	// If the user is not authenticated
 	if (!(await config.loggedIn())) {
+		console.log(`Trying to access ${route.path} while not authenticated`);
 		return redirect('/login');
 	}
 
